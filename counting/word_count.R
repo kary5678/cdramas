e3b052cb85c1369d829ab2df551e4ccf3c594022
kary5678 <60301055+kary5678@users.noreply.github.com>
2022-05-14 21:54:54 -0500

    Add word count analysis of titles

---
+++ counting/word_count.R
@@ -0,0 +1,16 @@
+cdramas_df <- read.csv("data/cdramas.csv", header = TRUE)
+cast_df <- read.csv("data/cdrama_casts.csv", header = TRUE)
+
+# Find the top 50 words used in drama titles
+title_words <- gsub("[^A-Za-z0-9\\' ]", " ", paste(cdramas_df$title_en, collapse=" ")) #single string of all words
+title_words <- unlist(strsplit(tolower(title_words), "\\s+")) #character vector of all words
+word_freq <- sort(table(title_words), decreasing = TRUE)
+output <- word_freq[1:50]
+cat(paste(rownames(output), output, sep=":"), sep = ", ")
+
+# Find the top words used in the native titles
+title_chars <- gsub("\\s+", "", paste(cdramas_df$title_zh, collapse = ""))
+title_chars <- unlist(strsplit(title_chars, split = ""))
+char_freq <- sort(table(title_chars), decreasing = TRUE)
+output2 <- char_freq[1:50]
+cat(paste(rownames(output2), output, sep=":"), sep = ", ")
