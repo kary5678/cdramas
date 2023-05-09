d44734908a8d69124d0168b466bebf283e1bce66
kary5678 <60301055+kary5678@users.noreply.github.com>
2023-05-09 18:56:11 -0500

    Remove visualizations directory

--- visualizations/visuals.R
+++
@@ -1,9 +0,0 @@
-ggplot(data=cdramas_df, aes(x=factor(year)))+
-  geom_bar()+
-  labs(title="Number of Chinese Dramas Released 2012-2021",
-       x="Year")
-
-ggplot(data=cdramas_df, aes(x=factor(year), y=eps))+
-  geom_boxplot()+
-  labs(title="Distribution of Chinese Drama Episode Counts 2012-2022",
-       x="Release Year", y="Episode Count")
